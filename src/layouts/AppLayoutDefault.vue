0ae3b60686b413b8dd4586cc62a3d16a2fd30c76
jadilovic <adilovic79@yahoo.com>
2022-06-09 23:17:18 +0200

    first commit

---
+++ src/layouts/AppLayoutDefault.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<h1>App Layout Default</h1>
+		<slot />
+	</div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+	{
+		path: '/',
+		name: 'Home',
+		component: () => import('@/views/Home.vue'),
+		meta: {
+			layout: 'AppLayoutHome',
+		},
+	},
+	{
+		path: '/about',
+		name: 'About',
+		component: () => import('@/views/About.vue'),
+		meta: {
+			layout: 'AppLayoutAbout',
+		},
+	},
+	{
+		path: '/contacts',
+		name: 'Contacts',
+		component: () => import('@/views/Contacts.vue'),
+		meta: {
+			layout: 'AppLayoutContacts',
+		},
+	},
+	{
+		path: '/test',
+		name: 'Test',
+		component: () => import('@/views/Home.vue'),
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
+});
 
-export default router
+export default router;
